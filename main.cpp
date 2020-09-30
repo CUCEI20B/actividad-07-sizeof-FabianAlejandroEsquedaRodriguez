25d480d95b3faa1c9e706083d1122d1524a2d74f
Fabián Alejandro Esqueda Rodríguez <fabian.esqueda2892@alumnos.udg.mx>
2020-09-29 20:07:22 -0500

    Comparar char, int, short y float

--- main.cpp
+++ main.cpp
@@ -8,6 +8,23 @@ int main() {
     getline(cin, cadena); cout<<endl;
     //Al meter un tipo de dato, se imprimira la cantidad de bytes que usa en memoria
     //Por ejemplo -> entrada: "int", salida: 4.
+
+    if(cadena == "char"){
+        cout<<sizeof(char)<<endl;
+    }
+    
+    else if(cadena == "int"){
+        cout<<sizeof(int)<<endl;
+    }
+    
+	else if(cadena == "short"){
+        cout<<sizeof(short)<<endl;
+    }
+    
+	else if(cadena == "float"){
+        cout<<sizeof(float)<<endl;
+    }
+
     
 
     return 0;
